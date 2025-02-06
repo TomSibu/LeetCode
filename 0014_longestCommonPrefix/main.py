a828eadd76c1c7a1d1cadf3e29efabee149ef9fc
Tom Sibu <118618366+TomSibu@users.noreply.github.com>
2025-02-06 22:02:45 +0530

    commit

--- 0014_longestCommonPrefix/main.py
+++
@@ -1,5 +0,0 @@
-from solution import Solution
-
-sol = Solution()
-print(sol.longestCommonPrefix(["flower", "flow", "flight"]))  # Output: "fl"
-print(sol.longestCommonPrefix(["dog", "racecar", "car"]))     # Output: ""

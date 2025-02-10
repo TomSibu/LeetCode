4b418b95ac0a3abfe6c592553448471e2ba35b3f
Tom Sibu <118618366+TomSibu@users.noreply.github.com>
2025-02-11 01:24:51 +0530

    Time: 0 ms (100%), Space: 17.8 MB (27.74%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.py
+++ 3447-clear-digits/3447-clear-digits.py
@@ -1,15 +1,9 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        chars = list(s)
-        to_remove = set()
-        
-        for i, ch in enumerate(chars):
-            if ch.isdigit():
-                to_remove.add(i)
-                for j in range(i - 1, -1, -1):
-                    if chars[j].isalpha() and j not in to_remove:
-                        to_remove.add(j)
-                        break  
-        
-        result = "".join([chars[i] for i in range(len(chars)) if i not in to_remove])
-        return result
+        s1=''
+        for i in s:
+            if i.isalpha():
+                s1+=i
+            else:
+                s1=s1[:-1]
+        return s1

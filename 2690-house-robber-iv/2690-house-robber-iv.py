d0b8b870ca0c5f6524d7f5f604c1429bbcf081d6
Tom Sibu <118618366+TomSibu@users.noreply.github.com>
2025-03-16 00:44:22 +0530

    Time: 351 ms (48.32%), Space: 28.6 MB (81.09%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.py
+++ 2690-house-robber-iv/2690-house-robber-iv.py
@@ -1,14 +1,12 @@
 class Solution:
     def minCapability(self, nums: List[int], k: int) -> int:
-        left=1
-        right=10**9
-        while left<right:
-            mid, t, i = (left+right)//2, 0, 0
-            while i< len(nums) and t<k:
-                if nums[i]<=mid:
-                    t+=1
-                    i+=2
-                else:
-                    i+=1
-            left, right = (mid+1, right) if t<k else (left,mid)
+        left, right = min(nums), max(nums)
+        while left < right:
+            mid, t, i = (left + right)//2, 0, 0
+            while i < len(nums):
+                if nums[i] <= mid:
+                    t += 1
+                    i += 1 
+                i += 1
+            left, right = (mid + 1, right) if t < k else (left, mid)
         return left

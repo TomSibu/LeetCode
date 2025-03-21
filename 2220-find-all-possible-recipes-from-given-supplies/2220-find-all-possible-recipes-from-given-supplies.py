116c429a32814fde324d002ffaa815b800d4585b
Tom Sibu <118618366+TomSibu@users.noreply.github.com>
2025-03-21 23:48:41 +0530

    Time: 142 ms (23.02%), Space: 20 MB (32.74%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        supply_set = set(supplies)
+        recipe_map = {recipe: set(ing) for recipe, ing in zip(recipes, ingredients)}
+        result = []
+
+        made_something = True
+        while made_something:
+            made_something = False
+            for recipe in list(recipe_map.keys()):
+                if recipe_map[recipe].issubset(supply_set):
+                    supply_set.add(recipe)
+                    result.append(recipe)
+                    del recipe_map[recipe] 
+                    made_something = True
+
+        return result
